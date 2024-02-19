c8dc5b0604bf884b60ea74de071c6c18dae1c4f9
wach0001 <wach0001@edu.mercantec.dk>
2024-02-19 11:11:54 +0100

    ud

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+Console.WriteLine(" ");
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line");
+
+Console.WriteLine('b');
+//Console.Write('Hello World!');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
